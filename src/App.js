fdd3756d57c8fa322a7cc6268c702d4516cca681
Bjorn Truyen <bjorn.truyen@aranealabs.com>
2019-12-12 19:14:04 +0100

    Added the basic components and geting overview pokemons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { ApolloProvider } from "react-apollo";
 import styled from "styled-components";
 import client from "./graphql";
 
+//Components
+import PokemonSelector from "./components/PokemonSelector";
+
 const Logo = styled.img`
   display: block;
   margin: 0 auto;
@@ -20,8 +23,7 @@ class App extends Component {
       <ApolloProvider client={client}>
         <Container>
           <Logo src="https://vignette.wikia.nocookie.net/logopedia/images/2/2b/Pokemon_2D_logo.svg/revision/latest/scale-to-width-down/639?cb=20170115063554" />
-
-          <div>BUILD YOUR APP here</div>
+          <PokemonSelector/>
         </Container>
       </ApolloProvider>
     );
